701c954e70d2a2ca660c45634ba93e8047172262
unknown <markovicanja98@gmail.com>
2020-04-18 17:36:43 +0200

    Popravka

--- js/schedule.js
+++ js/schedule.js
@@ -68,12 +68,6 @@ jQuery(document).ready(function($){
             //create the .event-date element for each event
             var durationLabel = '<span class="event-date">'+$(this).data('start')+' - '+$(this).data('end')+'</span>';
             $(this).children('a').prepend($(durationLabel));
-
-            //detect click on the event and open the modal
-            $(this).on('click', 'a', function(event){
-                event.preventDefault();
-                if( !self.animating ) self.openModal($(this));
-            });
         });
     };
 
