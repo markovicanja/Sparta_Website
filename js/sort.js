8afc1521d512df11f171a269bcd1c36c2342344c
unknown <markovicanja98@gmail.com>
2020-04-19 17:37:09 +0200

    Choose weights

--- js/sort.js
+++ js/sort.js
@@ -1,8 +1,9 @@
-function sortDivs(sortArr, first, second, third) {
+function sortDivs(sortArr, first, second, third, fourth) {
     for (i = 0; i < sortArr.length; i++) {
         if (sortArr[i]==first) $('.type1').attr("id","id"+ i);
         else if (sortArr[i]==second)  $('.type2').attr("id","id"+ i);
-        else  $('.type3').attr("id","id"+ i);
+        else if (sortArr[i]==third) $('.type3').attr("id","id"+ i);
+        else $('.type4').attr("id","id"+ i);
     }
 
     $('#id1').each(function() {
@@ -11,20 +12,25 @@ function sortDivs(sortArr, first, second, third) {
     $('#id2').each(function() {
         $(this).insertAfter($(this).parent().find('#id1'));
     });
+    if (fourth!='zzz' && fourth!=999) {
+        $('#id3').each(function() {
+            $(this).insertAfter($(this).parent().find('#id2'));
+        });
+    }
 }
 
 function sortInt(a, b) {
     return a-b;
 }
 
-function nameSort(first, second, third) {
-    var array = [first, second, third];
+function nameSort(first, second, third, fourth='zzz') {
+    var array = [first, second, third, fourth];
     var sortArr = array.sort();
-    sortDivs(sortArr, first, second, third);
+    sortDivs(sortArr, first, second, third, fourth);
 }
 
-function numSort(first, second, third) {
-    var array = [first, second, third];
+function numSort(first, second, third, fourth=999) {
+    var array = [first, second, third, fourth];
     var sortArr = array.sort(sortInt);
-    sortDivs(sortArr, first, second, third);
+    sortDivs(sortArr, first, second, third, fourth);
 }
