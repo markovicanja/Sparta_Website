1a23c02f594fde1cfc9b1bf0e11f5ea98e17a3da
unknown <markovicanja98@gmail.com>
2020-04-20 13:51:53 +0200

    Napravila galeriju

--- js/specificWorkout.js
+++ js/specificWorkout.js
@@ -1,6 +1,6 @@
-/* Gallery Modal */
+/* Image Modal */
 function openModal(id) {
-    var modal = document.getElementById("myModal");   
+    var modal = document.getElementById("image-modal");   
     var img = document.getElementById(id);
     var modalImg = document.getElementById("img");
     modal.style.display = "block";
@@ -8,6 +8,20 @@ function openModal(id) {
 }
 
 function closeModal() {
-    var modal = document.getElementById("myModal");  
+    var modal = document.getElementById("image-modal");  
     modal.style.display = "none";
 }
+
+/* Video Modal */
+$(document).ready(function() {
+    var $videoSrc;  
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data( "src" );
+    });
+    $('#myModal').on('shown.bs.modal', function (e) {      
+        $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+    })
+    $('#myModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src',$videoSrc); 
+    }) 
+});
