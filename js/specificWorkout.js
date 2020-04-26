4430c3582984dcce1eaee62d8c0e86608f4e79c3
vukasinstudge <vukasin.draskovic@gmai.com>
2020-04-27 09:36:55 +1000

    Rade ocene

--- js/specificWorkout.js
+++ js/specificWorkout.js
@@ -28,11 +28,26 @@ $(document).ready(function() {
 
 /* Rating stars */
 function setRating(workout, rating) {
-    sessionStorage.setItem(workout, rating);
+    ratings=JSON.parse(sessionStorage.getItem("ratings"));
+    workoutRating=ratings[workout];
+    // workoutRating.parseFloat(workoutRating);
+
+    if (rating>workoutRating) workoutRating+=0.1;
+    else if (rating<workoutRating) workoutRating-=0.1;
+    workoutRating=Math.round(workoutRating*10)/10;
+
+    $('#currRating').text(workoutRating+'/5');
+
+    ratings[workout]=workoutRating;
+    sessionStorage.setItem("ratings", JSON.stringify(ratings));
 }
 
 /* Check if user can rate this exercise */
 function canRate(workout) {
+    ratings=JSON.parse(sessionStorage.getItem("ratings"));
+    workoutRating=ratings[workout];
+    $('#currRating').text(workoutRating+'/5');
+
     userExercises=JSON.parse(sessionStorage.getItem("userExercises"));
     didExercise=false;
     for (i=0; i<userExercises.length; i++) if (userExercises[i]==workout) didExercise=true;
